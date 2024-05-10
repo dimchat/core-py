4829810b15824a728d8ec865159e7be202672c9a
Moky <albert.moky@gmail.com>
2024-05-10 23:44:43 +0800

    dimp v2.0.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import io
 
 from setuptools import setup, find_packages
 
-__version__ = '1.2.0'
+__version__ = '2.0.0'
 __author__ = 'Albert Moky'
 __contact__ = 'albert.moky@gmail.com'
 
@@ -38,7 +38,7 @@ setup(
         'Operating System :: OS Independent',
     ],
     install_requires=[
-        'dkd>=1.2.0',
-        'mkm>=1.2.0',
+        'dkd>=2.0.0',
+        'mkm>=2.0.0',
     ]
 )
